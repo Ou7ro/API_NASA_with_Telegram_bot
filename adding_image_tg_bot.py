e18a792eb7d11be9ec0168711a8d3976f2d2167b
Ou7ro <qew.qweqwe.2016@mail.ru>
2025-03-13 23:07:46 +0500

    publish photo to Telegram channel.

--- adding_image_tg_bot.py
+++ adding_image_tg_bot.py
@@ -5,6 +5,7 @@ import os
 
 load_dotenv()
 tg_token = os.environ['TG_TOKEN']
+tg_chat_id = os.environ['TG_CHAT_ID']
 bot = telegram.Bot(token=tg_token)
 
-bot.send_message(chat_id='@Nasa_testing_ground', text='Здарова бандиты')
+bot.send_document(chat_id=tg_chat_id, document='https://i.ytimg.com/vi/0Ep4Wo0RioA/maxresdefault.jpg')
