ad55a621002453612abedcaf3415315a39f0f803
Ou7ro <qew.qweqwe.2016@mail.ru>
2025-03-19 21:34:01 +0500

    Changed send_all_images_tg

--- send_all_images_tg.py
+++ send_all_images_tg.py
@@ -1,31 +1,27 @@
 from dotenv import load_dotenv
 import os
 from pathlib import Path
-import argparse
 from random import shuffle
 from time import sleep
 from environs import env
 from supporting_scripts import send_image
+from telegram.error import NetworkError
 
 
 def send_all_images(tg_token, tg_chat_id, seconds, file_names, dir_path):
     while True:
         for file_name in file_names:
             file_path = Path(dir_path) / file_name
-            send_image(tg_token, file_path, tg_chat_id)
-            sleep(seconds)
+            try:
+                send_image(tg_token, file_path, tg_chat_id)
+                sleep(seconds)
+            except NetworkError:
+                sleep(10)
+        shuffle(file_names)
 
 
-def send_only_one_images(tg_token, tg_chat_id, args, seconds, dir_path):
-    while True:
-        file_path = Path(dir_path) / args.name
-        send_image(tg_token, file_path, tg_chat_id)
-        sleep(seconds)
-
-
-def shuffle_file_names(path):
+def enumeration_file_names(path):
     for dirpath, dirnames, filenames in os.walk(path):
-        shuffle(filenames)
         return filenames
 
 
@@ -36,15 +32,8 @@ def main():
     seconds = env.int('TIME', default=14400)
     dir_path = env.str('DIRECTORY_PATH', default='images')
     Path(dir_path).mkdir(exist_ok=True)
-    parse = argparse.ArgumentParser(description='''Отправляет все имеющиеся фото, по умолчанию, каждые 4 часа.
-                                    Чтобы отправлять определенное фото, укажите в аргументе название файла''')
-    parse.add_argument('-n', '--name', help='image name')
-    args = parse.parse_args()
-    file_names = shuffle_file_names(dir_path)
-    if args.name is None:
-        send_all_images(tg_token, tg_chat_id, seconds, file_names, dir_path)
-    else:
-        send_only_one_images(tg_token, tg_chat_id, args, seconds, dir_path)
+    file_names = enumeration_file_names(dir_path)
+    send_all_images(tg_token, tg_chat_id, seconds, file_names, dir_path)
 
 
 if __name__ == '__main__':
